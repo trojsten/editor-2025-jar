19751397f32028fa1ed6182d2b792f9c9cecfb3a
Aiq0 <66842415+Aiq0@users.noreply.github.com>
2025-04-02 19:23:21 +0200

    feat: order tasks by points

--- src/routes/problems/+page.server.ts
+++ src/routes/problems/+page.server.ts
@@ -2,7 +2,17 @@ import db from '$lib/server/db';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
-	const problems = await db.problem.findMany({ select: { title: true, slug: true, points: true } });
+	const problems = await db.problem.findMany({
+		select: { title: true, slug: true, points: true },
+		orderBy: [
+			{
+				points: 'asc'
+			},
+			{
+				title: 'asc'
+			}
+		]
+	});
 	return {
 		problems
 	};
